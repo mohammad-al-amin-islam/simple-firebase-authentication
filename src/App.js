81358be4a233e6e55ecf114f41b99a4a4a3989ee
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-06 16:39:31 +0600

    git authentication added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from 'react';
 
 
@@ -10,10 +10,11 @@ function App() {
 
   const [user, setUser] = useState({})
 
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user
         setUser(user);
@@ -21,6 +22,17 @@ function App() {
       })
       .catch(error => {
         console.log('error', error);
+      })
+  }
+  const handleGitSignIn = () => {
+    signInWithPopup(auth, gitProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user)
+      })
+      .catch(error => {
+        console.log(error);
       })
   }
 
@@ -36,7 +48,11 @@ function App() {
   return (
     <div className="App">
       {
-        user.email ? <button onClick={handleSignOut}>Google Sign Out</button> : <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        user.uid ? <button onClick={handleSignOut}>Google Sign Out</button> : <>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGitSignIn}>Git Sign In</button>
+
+        </>
       }
 
 
